51507eee40bb569593ad8f3c35095a0b0490fd78
cbl6499 <clemens.blecha@students.fhv.at>
2017-10-28 17:01:45 +0200

    Splitpipe, aber noch nicht ganz fertig da copy noch fehlt

---
+++ src/Exercise1/SplitPipe.java
@@ -0,0 +1,64 @@
+package Exercise1;
+
+import pmp.interfaces.IOable;
+import pmp.interfaces.Readable;
+import pmp.interfaces.Writeable;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StreamCorruptedException;
+import java.security.InvalidParameterException;
+
+/**
+ * Created by ClemensB on 28.10.17.
+ */
+public class SplitPipe<T> implements IOable<T, T> {
+
+    private Readable<T> m_Input = null;
+    private Writeable<T> m_Output = null;
+    private Writeable<T> m_Output2 = null;
+
+    public SplitPipe(Readable<T> input)   {
+        if (input == null){
+            throw new InvalidParameterException("input filter can't be null!");
+        }
+        m_Input = input;
+    }
+
+    public SplitPipe(Writeable<T> output, Writeable<T> output2)   {
+        if (output == null || output2 == null){
+            throw new InvalidParameterException("output filter can't be null!");
+        }
+        m_Output = output;
+        m_Output2 = output2;
+    }
+
+    public SplitPipe(Readable<T> input, Writeable<T> output, Writeable<T> output2)   {
+        if (output == null || output2 == null){
+            throw new InvalidParameterException("output filter can't be null!");
+        }
+        m_Output = output;
+        m_Output2 = output2;
+
+        if (input == null){
+            throw new InvalidParameterException("input filter can't be null!");
+        }
+        m_Input = input;
+    }
+
+    public T read() throws StreamCorruptedException, FileNotFoundException {
+        if ( m_Input == null )
+            throw new InvalidParameterException("input filter can't be null!");
+
+        return m_Input.read();
+    }
+
+    public void write(T input) throws IOException {
+        if ( m_Output == null || m_Output2 == null )
+            throw new InvalidParameterException("output filter can't be null!");
+
+        m_Output.write(input);
+        //m_Output2.write(input.copy());  // muss noch ausprogrammiert werden
+    }
+
+}
