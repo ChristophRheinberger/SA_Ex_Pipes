b5153405434fb3a29336d253b88e9344328a8378
ChristophRheinberger <crh6731@students.fhv.at>
2017-12-01 19:02:38 +0100

    robot v2.1

--- src/Exercise4/controllers/ePuckAufgabeA.java
+++ src/Exercise4/controllers/ePuckAufgabeA.java
@@ -3,11 +3,16 @@ package Exercise4.controllers;
 import com.cyberbotics.webots.controller.DifferentialWheels;
 import com.cyberbotics.webots.controller.LightSensor;
 
+
+/**
+ * Created by Christoph on 27.11.2017.
+ */
 public class ePuckAufgabeA extends DifferentialWheels {
 
     private static int TIME_STEP = 15;
 
-    private static int MAX_SENSOR_VALUE = 3500;
+    private static int MAX_SENSOR_VALUE = 500;
+    private static int LIGHT_SENSOR_VALUE = 400;
 
     private static int S_FRONT_LEFT = 0; // Sensor front left
     private static int S_FRONT_RIGHT = 1; // Sensor front right
@@ -35,11 +40,14 @@ public class ePuckAufgabeA extends DifferentialWheels {
      * BangBangFollowTheWall class
      */
     public void run() {
+
         while (step(TIME_STEP) != -1) {
-            if (sensors[S_FRONT_LEFT].getValue() > MAX_SENSOR_VALUE
-                    && sensors[S_FRONT_RIGHT].getValue() > MAX_SENSOR_VALUE) {
-                if( lightSensorIsEqual()) {
+            if (sensors[S_FRONT_LEFT].getValue() < LIGHT_SENSOR_VALUE
+                    && sensors[S_FRONT_RIGHT].getValue() < LIGHT_SENSOR_VALUE) {
+                if( lightSensorValuesInTolerance() ) {
                     driveForward();
+                } else {
+                    stop();
                 }
             } else {
                 stop();
@@ -47,10 +55,12 @@ public class ePuckAufgabeA extends DifferentialWheels {
             System.out.println("ls7: " + sensors[S_FRONT_LEFT].getValue());
             System.out.println("ls0: " + sensors[S_FRONT_RIGHT].getValue());
         }
+
     }
 
-    private boolean lightSensorIsEqual() {
-        if(Math.abs(sensors[S_FRONT_LEFT].getValue() - Math.abs(sensors[S_FRONT_RIGHT].getValue())) < 25 ) {
+    private boolean lightSensorValuesInTolerance() {
+        if (Math.abs(sensors[S_FRONT_LEFT].getValue() - Math.abs(sensors[S_FRONT_RIGHT].getValue())) < LIGHT_SENSOR_VALUE ) {
+            System.out.println(Math.abs(sensors[S_FRONT_LEFT].getValue() - Math.abs(sensors[S_FRONT_RIGHT].getValue())));
             return true;
         } else {
             return false;
@@ -93,8 +103,6 @@ public class ePuckAufgabeA extends DifferentialWheels {
         ePuckAufgabeA controller = new ePuckAufgabeA();
         controller.run();
     }
-
-
 }
 
 
