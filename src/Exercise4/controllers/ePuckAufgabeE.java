8f86b21e6c89541cfe8e966b6bc2ae612a7f98fb
ChristophRheinberger <crh6731@students.fhv.at>
2017-12-05 13:22:57 +0100

    robot v2.2 wall following v1

--- src/Exercise4/controllers/ePuckAufgabeE.java
+++ src/Exercise4/controllers/ePuckAufgabeE.java
@@ -1,7 +1,99 @@
 package Exercise4.controllers;
 
+import com.cyberbotics.webots.controller.DifferentialWheels;
+import com.cyberbotics.webots.controller.DistanceSensor;
+
 /**
  * Created by ClemensB on 04.12.17.
  */
-public class ePuckAufgabeE {
+public class ePuckAufgabeE  extends DifferentialWheels {
+    private static int TIME_STEP = 15;
+
+    private static int MAX_SENSOR_VALUE = 150;
+    private static int DODGE_SENSOR_VALUE = 100;
+
+    private static int S_LEFT = 0; // Sensor left
+    private static int S_MIDDLE_LEFT = 1;  // Sensor middle left
+    private static int S_FRONT_LEFT = 2; // Sensor front left
+    private static int S_FRONT_RIGHT = 3; // Sensor front right
+    private static int S_MIDDLE_RIGHT = 4;  // Sensor middle right
+    private static int S_RIGHT = 5; // Sensor left
+    private static int MIN_SPEED = 0; // min. motor speed
+    private static int MAX_SPEED = 1000; // max. motor speed
+
+
+    private DistanceSensor[] sensors; // Array with all distance sensors
+
+    /**
+     * Constructor
+     */
+    public ePuckAufgabeE() {
+        super();
+        // get distance sensors and save them in array
+        sensors = new DistanceSensor[] { getDistanceSensor("ps5"), getDistanceSensor("ps6"),
+                getDistanceSensor("ps7"), getDistanceSensor("ps0"), getDistanceSensor("ps1"),
+                getDistanceSensor("ps2") };
+        for (int i=0; i<6; i++)
+            sensors[i].enable(10);
+    }
+
+    /**
+     * User defined function for initializing and running the
+     * BangBangFollowTheWall class
+     */
+    public void run() {
+        while (step(TIME_STEP) != -1) {
+            System.out.println("Left: " + sensors[S_FRONT_LEFT].getValue());
+            System.out.println("Right: " + sensors[S_FRONT_RIGHT].getValue());
+            if (sensors[S_FRONT_LEFT].getValue() > MAX_SENSOR_VALUE
+                    || sensors[S_MIDDLE_LEFT].getValue() > MAX_SENSOR_VALUE
+                    || sensors[S_FRONT_RIGHT].getValue() > MAX_SENSOR_VALUE) {
+                // drive right - reached a wall
+                driveRight();
+                System.out.println("Drive Right");
+            } else if (sensors[S_LEFT].getValue() < DODGE_SENSOR_VALUE) {
+                // drive forward if nothing is in front of the robot
+                driveLeft();
+                System.out.println("Drive Left");
+            } else {
+                driveForward();
+                System.out.println("Drive Forward");
+            }
+            //System.out.println("ps7: " + sensors[S_FRONT_RIGHT].getValue());
+        }
+
+    }
+
+    /**
+     * Robot drives to the right
+     */
+    private void driveRight() {
+        setSpeed(MAX_SPEED, MIN_SPEED);
+    }
+
+    /**
+     * Robot drives to the left
+     */
+    private void driveLeft() {
+        setSpeed(MIN_SPEED, MAX_SPEED);
+    }
+
+    /**
+     * Robot drives forward
+     */
+    private void driveForward() {
+        setSpeed(MAX_SPEED, MAX_SPEED);
+    }
+
+
+    /**
+     * Main method - in this method an instance of the controller is created and
+     * the method to launch the robot is called.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ePuckAufgabeE controller = new ePuckAufgabeE();
+        controller.run();
+    }
 }
