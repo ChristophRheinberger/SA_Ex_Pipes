cf3d92089a7b947169a82b36c9e5f5c7d3d23331
ChristophRheinberger <crh6731@students.fhv.at>
2017-12-10 17:04:30 +0100

    proportionalC

--- src/Exercise4/controllers/ePuckProportionalB.java
+++ src/Exercise4/controllers/ePuckProportionalB.java
@@ -1,9 +1,8 @@
 package Exercise4.controllers;
 
 import com.cyberbotics.webots.controller.DifferentialWheels;
+import com.cyberbotics.webots.controller.DistanceSensor;
 import com.cyberbotics.webots.controller.LightSensor;
-
-
 
 public class ePuckProportionalB extends DifferentialWheels {
 
@@ -11,21 +10,28 @@ public class ePuckProportionalB extends DifferentialWheels {
 
     private static int MAX_SPEED = 1000; // max. motor speed
 
-    private static double[] priorities= {1, 0.1, 0.2, 0.5, 0.5, 0.2, 0.1, 1};
+    private static double[] prioritiesLight = {1, 0.1, 0.2, 0.5, 0.5, 0.2, 0.1, 1};
+    private static double[] prioritiesDistance = {1, 1};
     private static double speedLeft;
     private static double speedRight;
     private boolean found = true;
 
 
-    private LightSensor[] sensors; // Array with all distance sensors
+    private LightSensor[] sensorsLight; // Array with all light sensors
+    private DistanceSensor[] sensorsDistance; // Array with all distance sensors
 
     public ePuckProportionalB() {
         super();
 
-        sensors = new LightSensor[] { getLightSensor("ls7"), getLightSensor("ls6"), getLightSensor("ls5"), getLightSensor("ls4"),
+        sensorsLight = new LightSensor[] { getLightSensor("ls7"), getLightSensor("ls6"), getLightSensor("ls5"), getLightSensor("ls4"),
                 getLightSensor("ls3"), getLightSensor("ls2"), getLightSensor("ls1"), getLightSensor("ls0") };
-        for (int i=0; i<8; i++)
-            sensors[i].enable(10);
+        for (int i=0; i<sensorsLight.length; i++)
+            sensorsLight[i].enable(10);
+
+        sensorsDistance = new DistanceSensor[] { getDistanceSensor("ps7"), getDistanceSensor("ps0") };
+
+        for (int i=0; i<sensorsDistance.length; i++)
+            sensorsDistance[i].enable(10);
     }
 
     public void run() {
@@ -38,12 +44,19 @@ public class ePuckProportionalB extends DifferentialWheels {
 
     private void drive() {
         found = true;
-        System.out.println("RIGHT: " + sensors[7].getValue());
-        System.out.println("LEFT: " + sensors[0].getValue());
+        System.out.println("RIGHT: " + sensorsLight[7].getValue());
+        System.out.println("LEFT: " + sensorsLight[0].getValue());
 
-        speedLeft = ((priorities[0] * sensors[0].getValue()) + (priorities[1] * sensors[1].getValue()) + (priorities[2] * sensors[2].getValue()) + (priorities[3] * sensors[3].getValue())) / 4;
-        speedRight = ((priorities[7] * sensors[7].getValue()) + (priorities[6] * sensors[6].getValue()) + (priorities[5] * sensors[5].getValue()) + (priorities[4] * sensors[4].getValue())) / 4;
-
+        speedLeft = ((prioritiesLight[0] * sensorsLight[0].getValue())
+                    + (prioritiesLight[1] * sensorsLight[1].getValue())
+                    + (prioritiesLight[2] * sensorsLight[2].getValue())
+                    + (prioritiesLight[3] * sensorsLight[3].getValue())) / 4
+                    - (prioritiesDistance[0] * sensorsDistance[0].getValue());
+        speedRight = ((prioritiesLight[7] * sensorsLight[7].getValue())
+                    + (prioritiesLight[6] * sensorsLight[6].getValue())
+                    + (prioritiesLight[5] * sensorsLight[5].getValue())
+                    + (prioritiesLight[4] * sensorsLight[4].getValue())) / 4
+                    - (prioritiesDistance[1] * sensorsDistance[1].getValue());
 
         if(speedLeft >= 1000 && speedRight >= 1000){
             speedLeft = 0;
@@ -59,7 +72,7 @@ public class ePuckProportionalB extends DifferentialWheels {
             speedRight = MAX_SPEED;
         }
 
-        if(found && sensors[0].getValue() >= 440){
+        if(found && sensorsLight[0].getValue() >= 440){
             speedLeft = 0;
             speedRight = 0;
         }
